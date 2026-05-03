3e368205b49208f17b8e3bc3dab1694d6ad7685a
ShivamSingh15077 <shivamsingh4106@gmail.com>
2026-05-03 10:39:45 +0530

    Counter-Ui-project

---
+++ 09-useState/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState } from 'react';
+const App = () => {
+    
+const [num, setnum] = useState(0)
+
+    function increaseNum(){
+        setnum(num+1)
+        
+    }
+    function decreaseNum(){
+        setnum(num-1)
+    }
+    function jumpby5(){
+        setnum(num+5)
+    }
+
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={increaseNum}>increase</button>
+      <button onClick={decreaseNum}>decrease</button>
+      <button onClick={jumpby5}>+5</button>
+    </div>
+  )
+}
+
+export default App
