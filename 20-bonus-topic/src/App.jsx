e781eff4786ca0039e0d76d67502f55231a70a33
ShivamSingh15077 <shivamsingh4106@gmail.com>
2026-05-07 14:33:06 +0530

    Bonus Topic

---
+++ 20-bonus-topic/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { useState } from 'react'
+
+const App = () => {
+  const [Theme, setTheme] = useState('light')
+  return (
+    <div>
+      <h1>Theme is {Theme}</h1>
+      <Navbar Theme={Theme} setTheme={setTheme}/>
+    </div>
+  )
+}
+
+export default App
